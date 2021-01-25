30992deae7344ef8205661a31b81f49f9a978e08
Sahil Patel <Patelsaho@gmail.com>
2021-01-25 18:16:17 -0500

    Staging the server.js, content added for Schema and Seed file

---
+++ db/burgerSeeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('All-American Beef Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
